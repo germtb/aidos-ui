01d06b0209612d2d2996f0224c691ffb6c40ae67
Gerard Moreno-Torres Bertran <germtf@gmail.com>
2023-07-13 20:25:32 +0100

    remove jest

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-
-export default {
-    preset: 'ts-jest',
-    testEnvironment: 'node',
-    transform: {
-      '^.+\\.tsx?$': 'ts-jest',
-    },
-    transformIgnorePatterns: ['<rootDir>/node_modules/'],
-  };
