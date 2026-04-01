9a4b4046b83fe4434edd1d62b0809d89c9090af0
Gerard Moreno-Torres Bertran <germtf@gmail.com>
2026-04-01 10:33:17 +0100

    temp

--- src/IconButton.tsx
+++ src/IconButton.tsx
@@ -4,28 +4,46 @@ import { Icon } from "./Icon";
 import { IconType } from "./IconType";
 import { getGlyphColor } from "./Interactable";
 import { Size } from "./jss";
+import { Span } from "./Text";
 
-const sizes = {
+const sizes: {
+  [key in Size]: number;
+} = {
   xsmall: 20,
   small: 24,
   medium: 32,
   large: 40,
   xlarge: 48,
+  xxlarge: 56,
+  xxxlarge: 64,
 };
 
 export interface IconButtonProps extends BaseButtonProps {
   icon: IconType;
   size: Size;
+  label?: string;
+  labelSize?: Size;
 }
 
 export const IconButton = React.forwardRef(
   (
-    { icon, size, color, bare, disabled, jss, ...buttonProps }: IconButtonProps,
+    {
+      icon,
+      size,
+      color,
+      bare,
+      disabled,
+      jss,
+      label,
+      labelSize = "medium",
+      ...buttonProps
+    }: IconButtonProps,
     ref?: React.Ref<HTMLButtonElement>
   ) => {
     return (
       <BaseButton
         {...buttonProps}
+        aria-label={buttonProps["aria-label"] ?? label}
         bare={bare}
         jss={[
           {
@@ -52,6 +70,11 @@ export const IconButton = React.forwardRef(
           icon={icon}
           color={getGlyphColor(color, disabled, bare)}
         />
+        {label && (
+          <Span size={labelSize} color={getGlyphColor(color, disabled, bare)}>
+            {label}
+          </Span>
+        )}
       </BaseButton>
     );
   }
